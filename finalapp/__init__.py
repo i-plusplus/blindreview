5bdcbf53b23840ebe8091ddc33c5ff6aa0f0bd40
Paras Malik <masterofmasters22@gmail.com>
2024-12-31 13:31:04 +0530

    adding db

--- finalapp/__init__.py
+++ finalapp/__init__.py
@@ -12,7 +12,8 @@ def create_app():
 
     # specify the context. like with app
     db.init_app(app)
-
+    with app.app_context():
+        db.create_all()
     # setup the login manager.
     login_manager = LoginManager()
     login_manager.login_view = 'auth.login'
